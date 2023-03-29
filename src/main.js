eac446a173f4cabd77a0715fc3c636aeef58a8f5
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-03-29 15:21:58 +0200

    scaffolding

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+
+// Import CSS   
+import './assets/scss/main.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+import App from './App.vue'
+
+createApp(App).mount('#app')
